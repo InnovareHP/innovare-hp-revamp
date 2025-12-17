bbf2786d22f6cb67355eb3a7abd3ed9a120c3f6d
redcloud442 <markivor.glorioso@gmail.com>
2025-12-18 02:51:07 +0800

    fix google env

--- lib/google.ts
+++ lib/google.ts
@@ -1,7 +1,17 @@
 import { JWT } from "google-auth-library";
 
+const base64EncodedServiceAccount = process.env.GSC_PRIVATE_KEY;
+if (!base64EncodedServiceAccount) {
+  throw new Error("GSC_PRIVATE_KEY is not set");
+}
+const decodedServiceAccount = Buffer.from(
+  base64EncodedServiceAccount,
+  "base64"
+).toString("utf-8");
+const credentials = JSON.parse(decodedServiceAccount);
+
 export const auth = new JWT({
   email: process.env.GSC_CLIENT_EMAIL,
-  key: process.env.GSC_PRIVATE_KEY?.split(String.raw`\n`).join("\n"),
+  key: credentials,
   scopes: ["https://www.googleapis.com/auth/webmasters.readonly"],
 });

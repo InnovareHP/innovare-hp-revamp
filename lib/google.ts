fb7e6b65bcd6d47f4a23e2046d395890d44b55c1
redcloud442 <markivor.glorioso@gmail.com>
2025-12-18 02:45:35 +0800

    add seo dashboard

--- lib/google.ts
+++ lib/google.ts
@@ -2,6 +2,6 @@ import { JWT } from "google-auth-library";
 
 export const auth = new JWT({
   email: process.env.GSC_CLIENT_EMAIL,
-  key: process.env.GSC_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  key: process.env.GSC_PRIVATE_KEY?.split(String.raw`\n`).join("\n"),
   scopes: ["https://www.googleapis.com/auth/webmasters.readonly"],
 });

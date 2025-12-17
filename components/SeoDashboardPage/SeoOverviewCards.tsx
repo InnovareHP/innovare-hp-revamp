73824ebf25b7fd9e8be701ae55e7235c3dda24df
redcloud442 <markivor.glorioso@gmail.com>
2025-12-18 02:55:28 +0800

    add dashboard

--- components/SeoDashboardPage/SeoOverviewCards.tsx
+++ components/SeoDashboardPage/SeoOverviewCards.tsx
@@ -40,7 +40,7 @@ export function SeoOverviewCards() {
 
   if (isLoading) {
     return (
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {[...Array(4)].map((_, i) => (
           <div
             key={i}

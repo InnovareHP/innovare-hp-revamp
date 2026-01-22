d786c6649373c71981bdc1b6081c40b3fc128ef5
redcloud442 <cloudred442@gmail.com>
2026-01-22 23:01:19 +0800

    fix bug

--- lib/storage.ts
+++ lib/storage.ts
@@ -3,11 +3,11 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
   forcePathStyle: true,
-  region: process.env.AWS_REGION!,
+  region: process.env.AWS_REGION_LOC!,
   endpoint: process.env.AWS_ENDPOINT!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_ID!,
+    secretAccessKey: process.env.AWS_SECRET_KEY!,
   },
 });
 export const uploadFile = async (file: File) => {

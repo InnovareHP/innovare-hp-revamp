86a0b9f223d9a012fb2ee4aac37c4e828a8ef402
redcloud442 <cloudred442@gmail.com>
2026-03-26 01:42:17 +0800

    initial commit

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,6 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY is missing");
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2026-01-28.clover",
   typescript: true,
 });
